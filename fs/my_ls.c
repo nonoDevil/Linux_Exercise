8b1ab4d21c5bf93a505e8ec367d2ec6cd48820ed
nonoDevil <linux.kakit@gmail.com>
2013-04-08 08:55:46 +0800

    init my_ls.c

---
+++ fs/my_ls.c
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  my_ls.c
+ *
+ *    Description:  Realize my ls command
+ *
+ *        Version:  1.0
+ *        Created:  2013年04月08日 08时51分07秒
+ *       Revision:  
+ *       Compiler:  gcc
+ *
+ *         Author:  nonoDevil, linux.kakit@gmail.com
+ *        Company:  Xiyou Linux Group
+ *
+ * =====================================================================================
+ */
+
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+
+
+	return EXIT_SUCCESS;
+}
+
+
