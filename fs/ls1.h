26b83cd71d42e7482d5c37c80008fafed8c0eaa5
nonoDevil <linux.kakit@gmail.com>
2013-04-08 11:57:28 +0800

    增加ls1.h头文件

---
+++ fs/ls1.h
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ls1.h
+ *
+ *    Description:  ls1的头文件 
+ *
+ *        Version:  1.0
+ *        Created:  2013年04月08日 10时16分38秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  nonoDevil, linux.kakit@gmail.com
+ *        Company:  Xiyou Linux Group
+ *
+ * =====================================================================================
+ */
+
+#ifndef __LS1_H__
+#define __ls1_H__
+
+#define __DIR_NAME_LEN__	255
+#define __DIR_ARRAY_MAX__	1000
+
+#endif 
