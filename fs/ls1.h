7ab963c85ed12a24dcdc66af7ae7b68940958ead
nonoDevil <linux.kakit@gmail.com>
2013-04-08 12:41:55 +0800

    增加参数宏

--- fs/ls1.h
+++ fs/ls1.h
@@ -22,4 +22,12 @@
 #define __DIR_NAME_LEN__	255
 #define __FILE_ARRAY_MAX__	1000
 
+/*
+ * 默认是小写参数，如果参数为大写，则加上U(表示upper)
+ */
+#define __PAR_A__			1
+#define __PAR_L__			2
+#define __PAR_R__			4
+
+
 #endif 
