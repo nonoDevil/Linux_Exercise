481c6b03388b1d9a738b99b9e42e58bb9ead451c
nonoDevil <linux.kakit@gmail.com>
2013-04-09 12:16:23 +0800

    代码重构，实现display_dir()

--- fs/my_ls.c
+++ fs/my_ls.c
@@ -1,7 +1,7 @@
 /*
  * =====================================================================================
  *
- *       Filename:  ls1.c
+ *       file_name:  ls1.c
  *
  *    Description:  实现ls命令
  *
@@ -24,81 +24,48 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <linux/limits.h>
 #include "my_ls.h"
-
 
 #define DEBUG 1
 
-void do_ls(const char *dirname);
-void print_err();
-void print_format();
 int handle_parameter(int argc, char *argv[]);
+void display_dir(char *path_name);
+void display_file(char *path_name);
+void display_sigle(char *file_name);
+void display_attribute(struct stat *buf, char *file_name);
+
+void do_ls(const char *dirname);
+void print_error();
+void print_format();
 static  int cmpstring(const void *p1, const void *p2);
 
 
 int main(int argc, char *argv[])
 {
-	char dirname[__FILE_NAME_LEN__];
-	char path[__FILE_NAME_LEN__];
+	char path_name[__FILE_NAME_LEN__];
 	int  param_count = 0;
 
-	/*处理"-"，返回"-的个数"*/
+	/*处理"-"，返回"第一个字符为"-"的参数个数"*/
 	param_count = handle_parameter(argc, argv);
+	
+#ifdef DEBUG
+	printf("param_count: %d\n", param_count);
+#endif
 
+	/* 如果没有目标路径，那么输出当前路径的内容 */
 	if (param_count == (argc - 1)) {
-		strcpy(dirname, "./");
-	} else {
-		strcpy(dirname, argv[1]);
+		printf("I am in\n");
+		strcpy(path_name, "./");
+		display_dir(path_name);	
 	}
 
-	do_ls(dirname);
+
+
+	//do_ls(dirname);
 
 	return EXIT_SUCCESS;
-}
-
-/*
- * Date: 2013/4/8
- * Description: 打印目录
- */
-void do_ls(const char *dirname)
-{
-	DIR *dir_ptr = NULL;
-	struct dirent *dir_cur = NULL;
-	char filename[__FILE_COUNT_MAX__][__FILE_NAME_LEN__]; /*[1000][255]*/ 
-	int count = 0;	/*统计目录下的文件数*/
-	int i = 0;
-
-	if ((dir_ptr = opendir(dirname)) == NULL) {
-		print_err("opendir", __LINE__);
-	} else {
-		while ((dir_cur = readdir(dir_ptr)) != NULL) {
-			strncpy(filename[count], dir_cur->d_name, strlen(dir_cur->d_name));
-			count++;
-		}
-		closedir(dir_ptr);
-	}
-	
-	printf("sizeof(filename[0]) = %d\n", sizeof(filename[0]));
-	qsort(filename, count, sizeof(filename[0]), cmpstring);
-
-	for (i = 0; i < count; i++) {
-		printf("%s\n", filename[i]);
-	}
-
-#ifdef DEBUG
-	printf("the total file number is %d\n", count);
-#endif 
-
-	return ;
-}
-
-/*
- * Date: 2013/4/8
- * Description: qsort()函数中的比较函数, 按字典顺序进行排序
- */
-static int cmpstring(const void *p1, const void *p2)
-{
-	return strcmp((char *)p1, (char *)p2);
 }
 
 /*
@@ -116,13 +83,9 @@ int handle_parameter(int argc, char *argv[])
 	 * argc < 2 说明命令是 $ ls 
 	 * 列出当前目录下的文件/目录，并且不打印隐藏文件
 	 */
-	if (argc < 2) {
-		return ;
-	}
 
-	/*
-	 * 解析命令，存入param数组
-	 */
+	/* 解析命令，存入param数组*/
+	k = 0;
 	for (i = 1; i < argc; i++) {
 		if (argv[i][0] == '-') {
 			for (j = 1; j < strlen(argv[i]); j++, k++) {
@@ -139,10 +102,8 @@ int handle_parameter(int argc, char *argv[])
 	printf("\n");
 #endif
 
-	/*
-	 * 根据param[]设置命令 eg: -l -a
-	 */
-	for (i = 0; i < j; i++) {
+	/* 根据param[]设置命令 eg: -l -a */
+	for (i = 0; i < k; i++) {
 		if (param[i] == 'a') {
 			g_parameter |= __PARAM_A__;
 		} else if (param[i] == 'l') {
@@ -162,7 +123,7 @@ int handle_parameter(int argc, char *argv[])
 		} else if (param[i] == 'q') {
 			g_parameter |= __PARAM_Q__;
 		} else {
-			printf("Can not recognise the parameter \"%c\"\n", param[i]);
+			printf("Can not recognize the parameter \"%c\"\n", param[i]);
 			printf("Please try to ls --help to get more help.\n");
 			exit(0);
 		}
@@ -171,16 +132,115 @@ int handle_parameter(int argc, char *argv[])
 	return param_count;
 }
 
+/*
+ * Date: 2013/4/9 Tues
+ * Description: 输出目录内容
+ * Parameters: path_name目录名
+ */
+void display_dir(char *path_name)
+{
+	DIR *dir = NULL;
+	struct dirent *dir_cur = NULL;
+	char file_name[__FILE_COUNT_MAX__][PATH_MAX+1];
+	int path_len = 0; 
+	int file_len = 0;
+	int count = 0;
+	int i = 0;
+
+	if ((dir = opendir(path_name)) == NULL) {
+		print_error("opendir", __LINE__);
+		exit(-1);
+	
+	
+	}
+	/*获取该目录下的所有文件名*/
+
+	while ((dir_cur = readdir(dir)) != NULL) {
+		path_len = strlen(path_name);
+		strncpy(file_name[count], path_name, path_len);
+		file_name[count][path_len] = '\0';
+		file_len = strlen(dir_cur->d_name);
+		strncat(file_name[count], dir_cur->d_name, file_len);		
+		count++;
+	}
+#ifdef DEBUG
+	for (i = 0; i < count; i++) {
+		printf("%s\n", file_name[i]);
+	}
+#endif
+	
+
+	return ;
+}
+void display_file(char *path_name)
+{
+	return ;
+}
+void display_sigle(char *file_name)
+{
+	return ;
+}
+void display_attribute(struct stat *buf, char *file_name)
+{
+	return ;
+}
+
+/*
+ * Date: 2013/4/8
+ * Description: 打印目录
+ */
+void do_ls(const char *dirname)
+{
+	DIR *dir_ptr = NULL;
+	struct dirent *dir_cur = NULL;
+	char file_name[__FILE_COUNT_MAX__][__FILE_NAME_LEN__]; /*[1000][255]*/ 
+	int count = 0;	/*统计目录下的文件数*/
+	int i = 0;
+
+	if ((dir_ptr = opendir(dirname)) == NULL) {
+		print_error("opendir", __LINE__);
+	} else {
+		while ((dir_cur = readdir(dir_ptr)) != NULL) {
+			strncpy(file_name[count], dir_cur->d_name, strlen(dir_cur->d_name));
+			count++;
+		}
+		closedir(dir_ptr);
+	}
+	
+	printf("sizeof(file_name[0]) = %d\n", sizeof(file_name[0]));
+	qsort(file_name, count, sizeof(file_name[0]), cmpstring);
+
+	for (i = 0; i < count; i++) {
+		printf("%s\n", file_name[i]);
+	}
+
+#ifdef DEBUG
+	printf("the total file number is %d\n", count);
+#endif 
+
+	return ;
+}
+
+/*
+ * Date: 2013/4/8
+ * Description: qsort()函数中的比较函数, 按字典顺序进行排序
+ */
+static int cmpstring(const void *p1, const void *p2)
+{
+	return strcmp((char *)p1, (char *)p2);
+}
+
 
 /*
  * Date: 2013/4/8
  * Description: 打印错误信息
  */
-void print_err(char *err_string, int line)
+void print_error(char *err_string, int line)
 {
 	fprintf(stderr, "line: %d\n", line);
 	perror(err_string);
-	exit(-1);
+
+	return ;
 }
 
 /*
