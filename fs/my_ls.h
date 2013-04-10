21fd73eee4857b2fdc13efda00f9b33b43b5de0d
nonoDevil <linux.kakit@gmail.com>
2013-04-10 21:57:25 +0800

    修改参数

--- fs/my_ls.h
+++ fs/my_ls.h
@@ -28,13 +28,14 @@
 #define __PARAM_A__			1		/*-a 打印出隐藏文件*/
 #define __PARAM_L__			2		/*-l 列出文件属性*/
 #define __PARAM_UR__		4		/*-R 递归输出目标路径下所有目录的内容*/
-#define __PARAM_R__         8		/*-r 逆序输出*/
+#define __PARAM_R__			8		/*-r 逆序输出*/
 #define __PARAM_U__			16		/* with -lt: sort by, and show, access time with  -l:  show  access time and sort by name otherwise: sort by access time*/
 #define __PARAM_I__			32		/*-i 打印出每个文件的inode*/
 #define __PARAM_T__			64		/*-t 根据修改时间修改*/
-#define __PARAM_US__			128		/*-s 根据文件大小排序*/
+#define __PARAM_US__		128		/*-s 根据文件大小排序*/
 #define __PARAM_Q__			256		/*-q 不排序输出*/
 
+/*判断是否含有某种参数的宏*/
 #define P_HASA(p)  ((p)&(__PARAM_A__))
 #define P_HASL(p)  ((p)&(__PARAM_L__))
 #define P_HASUR(p) ((p)&(__PARAM_UR__))
