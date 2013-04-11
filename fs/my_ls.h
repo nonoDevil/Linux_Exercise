6e1f1efd85f57a2ec06f9fe582b4d45cf30e7283
nonoDevil <linux.kakit@gmail.com>
2013-04-11 08:38:02 +0800

    优化代码风格

--- fs/my_ls.h
+++ fs/my_ls.h
@@ -1,9 +1,9 @@
 /*
  * =====================================================================================
  *
- *       Filename:  ls1.h
+ *       Filename:  my_ls.h
  *
- *    Description:  ls1的头文件 
+ *    Description:  my_ls的头文件 
  *
  *        Version:  1.0
  *        Created:  2013年04月08日 10时16分38秒
@@ -25,17 +25,17 @@
 /*
  * 默认是小写参数，如果参数为大写，则加上U(表示upper)
  */
-#define __PARAM_A__			1		/*-a 打印出隐藏文件*/
-#define __PARAM_L__			2		/*-l 列出文件属性*/
-#define __PARAM_UR__		4		/*-R 递归输出目标路径下所有目录的内容*/
-#define __PARAM_R__			8		/*-r 逆序输出*/
-#define __PARAM_U__			16		/* with -lt: sort by, and show, access time with  -l:  show  access time and sort by name otherwise: sort by access time*/
-#define __PARAM_I__			32		/*-i 打印出每个文件的inode*/
-#define __PARAM_T__			64		/*-t 根据修改时间修改*/
-#define __PARAM_US__		128		/*-s 根据文件大小排序*/
-#define __PARAM_Q__			256		/*-q 不排序输出*/
+#define __PARAM_A__         1		/*-a 打印出隐藏文件*/
+#define __PARAM_L__         2		/*-l 列出文件属性*/
+#define __PARAM_UR__        4		/*-R 递归输出目标路径下所有目录的内容*/
+#define __PARAM_R__         8		/*-r 逆序输出*/
+#define __PARAM_U__         16		/* with -lt: sort by, and show, access time with  -l:  show  access time and sort by name otherwise: sort by access time*/
+#define __PARAM_I__         32		/*-i 打印出每个文件的inode*/
+#define __PARAM_T__         64		/*-t 根据修改时间修改*/
+#define __PARAM_US__        128		/*-s 根据文件大小排序*/
+#define __PARAM_Q__         256		/*-q 不排序输出*/
 
-#define __S_IFMSB__			07000	/*mask of special bit*/
+#define __S_IFMSB__         07000	/*mask of special bit*/
 
 /*判断是否含有某种参数的宏*/
 #define P_HASA(p)  ((p)&(__PARAM_A__))
@@ -54,9 +54,9 @@
 /*终端行可显示字符数*/
 #define __ROW_LEN_MAX__			80
 
-static int g_parameter				= 0;				/*用来记录用户输入的ls - 的参数*/
-static int g_row_len_rest			= __ROW_LEN_MAX__;	/*终端一行剩余可显示字符数*/
-static int g_dir_longest_file_name	= 0;				/*目录文件下最长的文件名长度*/
-static int g_file_longest_file_name	= 0;		/*最长的文件名长度*/
+static int g_parameter              = 0;				/*用来记录用户输入的ls - 的参数*/
+static int g_row_len_rest           = __ROW_LEN_MAX__;	/*终端一行剩余可显示字符数*/
+static int g_dir_longest_file_name  = 0;				/*目录文件下最长的文件名长度*/
+static int g_file_longest_file_name = 0;		/*最长的文件名长度*/
 
 #endif 
